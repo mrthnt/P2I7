38af9905a667240041a8be714c7e8b31ec53c051
Maxchess67 <maxence.l2005@gmail.com>
2025-04-07 17:27:28 +0200

    méthode voisin_le_plus_proche
    Ajout de la méthode voisin_le_plus_proche ainsi que son test

--- projet.py
+++ projet.py
@@ -8,6 +8,19 @@ class Simulation :
         self.liste_de_poissons = liste_de_poissons
         self.liste_de_predateurs = liste_de_predateurs
 
+    def voisin_le_plus_proche(self, p, i): 			# i est l'indice temporel concerné de la matrice positions
+        poisson_1 = self.liste_de_poissons[p]		# p est l'indice du poisson concerné dans liste_de_poissons
+        poisson_voisin = None
+        distance_min = np.inf
+        for n in range(len(self.liste_de_poissons)):
+            if n != p :
+                poisson_2 = self.liste_de_poissons[n]
+                distance = poisson_1.distance(poisson_2, i)
+                if distance < distance_min :
+                    distance_min = distance
+                    poisson_voisin = poisson_2
+        return poisson_voisin
+
 
 
 class Boid :
@@ -37,6 +50,7 @@ class Boid :
         return norme
 
 
+
 class Poisson(Boid):
     
     def __init__(self, position_initiale, vitesse_initiale, R_attraction_poisson, R_repulsion_poisson, v_max, N, dt):
@@ -52,7 +66,17 @@ def test_de_classes():
     poisson1 = Poisson([0, 0, 1], [0, 0, 0], 10, 3, 20, 5, 0.1)
     poisson2 = Poisson([0, 2, 0], [0, 0, 0], 10, 3, 20, 5, 0.1)
     print(poisson1)
+    print()
     print(poisson2)
+    print()
 
     distance = poisson1.distance(poisson2, 0)
     print(distance) 
+    print()
+
+    poisson_voisin = la_simu.voisin_le_plus_proche(0, 0)
+    print(poisson_voisin)
+
+
+
+
