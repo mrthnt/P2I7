40589be3a224f792a83ca09c7beed67fa177fe40
mrthnt <hanneton.martin@gmail.com>
2025-04-09 08:51:41 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib.animation import FuncAnimation
 
 class Simulation :
     
