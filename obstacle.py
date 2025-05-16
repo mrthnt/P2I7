1c54fdc4eab3c99bae962fd0c24809cd153df353
mrthnt <hanneton.martin@gmail.com>
2025-05-16 17:46:35 +0200

    il manque plus que couper vision

--- obstacle.py
+++ obstacle.py
@@ -168,7 +168,7 @@ class Simulation :
         for obstacle in self.liste_obstacles:
             vecteur_normal, projete_tan = poisson.infos_distance(obstacle, i)
             projete_normal = np.linalg.norm(vecteur_normal)
-            if projete_normal <= obstacle.distance_repulsion and projete_tan<obstacle.longueur/2:
+            if projete_normal <= obstacle.distance_repulsion and abs(projete_tan)<obstacle.longueur/2:
                 res += obstacle.coefficient_repulsion*vecteur_normal/(np.linalg.norm(vecteur_normal))**2
         return res 
     
@@ -472,7 +472,7 @@ def test_3():
     distance_seuil = 100; alpha_cohesion = 20; alpha_separation = 10000; alpha_alignement = 10; a_rng = 60
     r_cohesion = 400; r_separation = 60; r_alignement = 5
     N = 500
-    obstacle = Obstacle([400,-20,1000,-30])
+    obstacle = Obstacle([180,-20,1000,10])
     liste_obstacle = [obstacle]
     poissons = []
     for i in range(25):
