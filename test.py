ec625aa57dc4ae491dfecdf158fcc0453c00f966
luc2115 <luc.bourgeoisj@gmail.com>
2025-05-26 15:24:49 +0200

    Ajout 2e test

--- test.py
+++ test.py
@@ -16,7 +16,49 @@ def test_3():
     fenetre = GUI(nouvelle_simu,1,500)
     print(f"le parametre d'ordre à 2 secondes est de {nouvelle_simu.moyennage_parametre_ordre(int(2.0/nouvelle_simu.dt),200)}")
 
-
+###ca fait une batiment avec 2 assaillants et 17 civils
+def test_3():
+    distance_seuil = 100; alpha_cohesion = 20; alpha_separation = 10000; alpha_alignement = 10; a_rng = 60
+    r_cohesion = 400; r_separation = 60; r_alignement = 5; r_predation = 600; r_proies = 700; 
+    N = 500
+    predateur = Predateur([0,0], [0,15], 600)
+    predateur2 = Predateur([6,0], [15,0], 600)
+    bord_gauche = Obstacle([-200,400,-200,-400])
+    bord_haut = Obstacle([-200,400,600,400])
+    bord_bas_gauche = Obstacle([-200,-400,100,-400])
+    bord_bas_droit = Obstacle([600,-400,300,-400])
+    bord_droit = Obstacle([600,400,600,-400])
+    centre_gauche_haut = Obstacle([0,50,100,150])
+    centre_gauche_bas = Obstacle([0,50,100,-50])
+    centre_droit_haut = Obstacle([200,50,100,150])
+    centre_droit_bas = Obstacle([200,50,100,-50])
+    ligne_bas = Obstacle([200,-200,600,-200])
+    liste_obstacle = [bord_bas_droit,bord_droit,bord_haut,bord_gauche,bord_bas_gauche,
+                      centre_droit_bas,centre_droit_haut,centre_gauche_haut,centre_gauche_bas,
+                      ligne_bas
+                      ]
+    poisson1 = Poisson([200,300], [5,0], 500)
+    poisson2 = Poisson([250,300], [5,0], 500)
+    poisson3 = Poisson([300,300], [5,0], 500)
+    poisson4 = Poisson([400,300], [5,0], 500)
+    poisson5 = Poisson([450,300], [5,0], 500)
+    poisson6 = Poisson([200,10], [5,0], 500)
+    poisson7 = Poisson([-200,0], [5,0], 500)
+    poisson12 = Poisson([-200,0], [5,0], 500)
+    poisson8 = Poisson([250,-300], [5,0], 500)
+    poisson9 = Poisson([400,-325], [5,0], 500)
+    poisson10 = Poisson([200,400], [5,0], 500)
+    poisson11 = Poisson([200,400], [5,0], 500)
+    poisson13 = Poisson([-150,-200],[3,1], 500)
+    poisson14 = Poisson([-100,-200],[-2,0], 500)
+    poisson15 = Poisson([-50,250],[3,0],500)
+    poisson16 = Poisson([-75,150], [0,-100], 500)
+    poisson17 = Poisson([0,5],[0,0],500)
+    poissons = [poisson1,poisson2,poisson3,poisson4,poisson5,poisson6,poisson7,poisson8,poisson9,poisson10,poisson11,poisson12,poisson13,poisson14,poisson15,poisson16]
+    nouvelle_simu = Simulation(poissons, [predateur,predateur2],liste_obstacle, N, 0.01, alpha_cohesion, alpha_separation, alpha_alignement, a_rng, r_cohesion, r_separation, r_alignement, r_predation, r_proies)
+    nouvelle_simu.calcul_tableaux()
+    fenetre = GUI(nouvelle_simu,1,500,False)
+    print(f"le parametre d'ordre à 2 secondes est de {nouvelle_simu.moyennage_parametre_ordre(int(2.0/nouvelle_simu.dt),200)}")
 
 
 
